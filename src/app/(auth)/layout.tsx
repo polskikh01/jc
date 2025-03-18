bc8cb7ca0843c182ec110f38aa5a6ad2f5b9688b
Evgeny Polskikh <polskikh01@gmail.com>
2025-03-18 15:28:58 +0400

    feat: up tailwindcss

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -16,7 +16,7 @@ const AuthLayout = ({children}: AuthLayoutProps) => {
 
     return (
         <main className="bg-neutral-100 min-h-screen">
-            <div className="mx-auto max-w-screen-2xl p-4">
+            <div className="mx-auto max-w-(--breakpoint-2xl) p-4">
                 <nav className="flex justify-between items-center">
                     <Image src="/logo.svg" height="56" width="152" alt="Logo" priority />
                     <Button
